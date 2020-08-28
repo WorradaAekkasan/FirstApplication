f7ec3017aeb9369ce81d2cbd8ddf404bb1bab439
worrada <worrada@users.noreply.github.com>
2020-08-28 15:41:09 +0700

    Create the DAO

---
+++ app/src/main/java/com/example/firstapplication/database/ContactDao.kt
@@ -0,0 +1,15 @@
+package com.example.firstapplication.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+
+
+@Dao
+interface ContactDao {
+    @Insert
+    fun insert(contact: Contact)
+    @Query("SELECT * from contact_table")
+    fun get(): LiveData<List<Contact>>
+}
