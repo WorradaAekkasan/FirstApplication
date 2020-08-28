26bbb84d3caf351c0b2541f46a279cb29e15a973
worrada <worrada@users.noreply.github.com>
2020-08-28 16:09:48 +0700

    Update ContactFragment.kt

--- app/src/main/java/com/example/firstapplication/ContactFragment.kt
+++ app/src/main/java/com/example/firstapplication/ContactFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
+import com.example.firstapplication.database.Database
 import com.example.firstapplication.databinding.FragmentContactBinding
 
 class ContactFragment : Fragment() {
@@ -21,7 +23,16 @@ class ContactFragment : Fragment() {
             container,
             false
         )
-        setHasOptionsMenu(true)
+        val application = requireNotNull(this.activity).application
+        val dataSource = Database.getInstance(application).contactDao
+        val viewModelFactory = ContactViewModelFactory(dataSource, binding, application)
+        val contactViewModel =
+            ViewModelProvider(
+                this, viewModelFactory
+            ).get(ContactViewModel::class.java)
+        binding.contactViewModel = contactViewModel
+        binding.lifecycleOwner = this
+        return binding.root
         return binding.root
     }
 
